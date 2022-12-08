a7f405c2423092a88c81a52b5b1e53dcb9b2156c
Hsa <gmltjs150@naver.com>
2022-12-08 14:00:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.header('Breakfast Menu🥣🥣🥣🥣')
+streamlit.text('🥗🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
